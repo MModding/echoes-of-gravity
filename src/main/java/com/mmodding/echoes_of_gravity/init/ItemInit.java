b789617023c67fb49fa9cbfdc85c9be3da8f0204
Max094Reikeb <maxencebekier@gmail.com>
2022-05-14 00:21:50 +0200

    Added init block and item classes

---
+++ src/main/java/com/mmodding/echoes_of_gravity/init/ItemInit.java
@@ -0,0 +1,9 @@
+package com.mmodding.echoes_of_gravity.init;
+
+public class ItemInit {
+	// FirstMegaGame4 jtm <3
+	// La phrase au dessus est fausse, et la phrase en dessous et vraie
+	// La phrase au dessus est fausse
+	// PARADOXE
+	// Bref ici c'est pour enregistrer les blocs quoi... (feur)
+}
