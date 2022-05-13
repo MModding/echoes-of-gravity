92494bd6be822efcf0d084d44cd13c833e9163e7
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-13 23:21:36 +0200

    Add all Files and Remove README.md

---
+++ src/main/java/com/mmodding/echoes_of_gravity/EchoesOfGravity.java
@@ -0,0 +1,15 @@
+package com.mmodding.echoes_of_gravity;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoesOfGravity implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Echoes of Gravity");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("{} - Initialization", mod.metadata().name());
+	}
+}
