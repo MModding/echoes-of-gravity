7a221811de63c1784447c29b1381875d85adc936
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2022-05-15 21:15:50 +0200

    I forgot that

--- src/main/java/com/mmodding/echoes_of_gravity/init/Entities.java
+++ src/main/java/com/mmodding/echoes_of_gravity/init/Entities.java
@@ -2,16 +2,16 @@ package com.mmodding.echoes_of_gravity.init;
 
 import com.mmodding.echoes_of_gravity.Utils;
 import com.mmodding.echoes_of_gravity.entities.OldDragonLightningEntity;
-import com.mmodding.mmodding_lib.lib.entities.CustomEntityType;
 import com.mmodding.mmodding_lib.lib.initializers.ElementsInitializer;
 import com.mmodding.mmodding_lib.lib.utils.RegistrationUtils;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 
 public class Entities implements ElementsInitializer {
 
-	public static final CustomEntityType<OldDragonLightningEntity> OLD_DRAGON_LIGHTNING = (CustomEntityType<OldDragonLightningEntity>) FabricEntityTypeBuilder
+	public static final EntityType<OldDragonLightningEntity> OLD_DRAGON_LIGHTNING = FabricEntityTypeBuilder
 			.create(SpawnGroup.MISC, OldDragonLightningEntity::new).disableSaving().dimensions(EntityDimensions.fixed(0.0F, 0.0F))
 			.trackRangeBlocks(16).trackedUpdateRate(Integer.MAX_VALUE).build();
 
